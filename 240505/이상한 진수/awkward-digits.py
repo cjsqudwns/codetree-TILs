c0d3ab49848eadf692ed1b52baa60573313142dd
Cheon Byeongjun <59379460+cjsqudwns@users.noreply.github.com>
2024-05-05 15:14:02 +0900

    Title: 이상한 진수, Time: 111ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240505/이상한 진수/awkward-digits.py
@@ -0,0 +1,33 @@
+import math
+import sys
+
+
+def cal(num, mul, ch_idx, ch_num):
+    result = 0
+    e = 0
+    while num >= 1:  # e : 0 ~~~
+        if e != ch_idx:
+            result += math.pow(mul, e) * (num % 10)
+        else:
+            result += math.pow(mul, e) * ch_num
+        num //= 10
+        e += 1
+    return int(result)
+
+
+# 입력 받기
+a = input()
+b = input()
+l = []
+for i in range(len(a)):  # 가장 높은 자리 수부터
+    if a[len(a) - i - 1] == '0':
+        l.append(cal(int(a), 2, i, 1))
+    else:
+        l.append(cal(int(a), 2, i, 0))
+for i in range(len(b)):
+    for j in range(3):
+        if j != b[len(b) - i - 1]:
+            ans = cal(int(b), 3, i, j)
+            if ans in l:
+                print(ans)
+                sys.exit()
