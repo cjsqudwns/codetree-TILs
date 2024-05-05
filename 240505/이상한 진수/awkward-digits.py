b8655cb9727d9809bfafc9ea5695d6c8a77c75b8
Cheon Byeongjun <59379460+cjsqudwns@users.noreply.github.com>
2024-05-05 15:29:51 +0900

    Title: 이상한 진수, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240505/이상한 진수/awkward-digits.py
+++ 240505/이상한 진수/awkward-digits.py
@@ -2,10 +2,10 @@ import math
 import sys
 
 
-def cal(num, mul, ch_idx, ch_num):
+def cal(num, num_len, mul, ch_idx, ch_num):
     result = 0
     e = 0
-    while num >= 1:  # e : 0 ~~~
+    for _ in range(num_len):
         if e != ch_idx:
             result += math.pow(mul, e) * (num % 10)
         else:
@@ -19,15 +19,15 @@ def cal(num, mul, ch_idx, ch_num):
 a = input()
 b = input()
 l = []
-for i in range(len(a)):  # 가장 높은 자리 수부터
+for i in range(len(a)):  # 가장 낮은 자리 수부터
     if a[len(a) - i - 1] == '0':
-        l.append(cal(int(a), 2, i, 1))
+        l.append(cal(int(a), len(a), 2, i, 1))
     else:
-        l.append(cal(int(a), 2, i, 0))
+        l.append(cal(int(a), len(a), 2, i, 0))
 for i in range(len(b)):
     for j in range(3):
-        if j != b[len(b) - i - 1]:
-            ans = cal(int(b), 3, i, j)
+        if str(j) != b[len(b) - i - 1]:
+            ans = cal(int(b), len(b), 3, i, j)
             if ans in l:
                 print(ans)
                 sys.exit()
