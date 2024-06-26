88cc379907367eca3311dc2b03534c89597fc22a
Cheon Byeongjun <59379460+cjsqudwns@users.noreply.github.com>
2024-06-27 01:14:55 +0900

    Title: Carry 피하기, Time: 5071ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

---
+++ 240627/Carry 피하기/escaping-carry.py
@@ -0,0 +1,30 @@
+def check_carry(n1, n2):
+    while n1 > 0 or n2 > 0:
+        # print("Before\nn1 : %d\nn2 : %d" % (n1, n2))
+        c1 = n1 % 10
+        c2 = n2 % 10
+        if c1 + c2 >= 10:
+            return True
+        n1 /= 10
+        n2 /= 10
+        # print("After\nn1 : %d\nn2 : %d" % (n1, n2))
+    return False
+
+
+def backtracking(cnt, depth, sum):
+    global result
+    result = max(cnt, result)
+    if depth == n:
+        return
+    for i in range(depth, n):
+        if not check_carry(sum, num_arr[i]):
+            backtracking(cnt + 1, i + 1, sum + num_arr[i])
+
+
+n = int(input())
+num_arr = []
+for _ in range(n):
+    num_arr.append(int(input()))
+result = 0
+backtracking(0, 0, 0)
+print(result)
