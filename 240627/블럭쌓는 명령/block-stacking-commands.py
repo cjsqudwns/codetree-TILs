cf520b469953e30ee7e9a5386a0ed12c2f1f54bf
Cheon Byeongjun <59379460+cjsqudwns@users.noreply.github.com>
2024-06-27 02:30:44 +0900

    Title: 블럭쌓는 명령, Time: 5021ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

---
+++ 240627/블럭쌓는 명령/block-stacking-commands.py
@@ -0,0 +1,8 @@
+n, k = map(int, input().split())
+arr = [0 for _ in range(n)]
+for i in range(k):
+    a, b = map(int, input().split())
+    for j in range(a, b + 1):
+        arr[j - 1] += 1
+arr.sort()
+print(arr[int(n / 2)])
