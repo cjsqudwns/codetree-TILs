ba80dbb8c96d608850c2938621dc882efced8c00
Cheon Byeongjun <59379460+cjsqudwns@users.noreply.github.com>
2024-06-29 04:18:00 +0900

    Title: 무~, Time: 994ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240629/무~/moo.py
+++ 240629/무~/moo.py
@@ -1,13 +1,18 @@
-def calculate_s(depth, before_str):
-    result = before_str
-    if len(result) >= n:
-        print(result[n - 1])
+def calculate_s(before_m, before_mid_len):
+    m_locate = before_m
+    str_length = m_locate[-1] + 3
+    if str_length >= n:
+        if n - 1 in m_locate:
+            print("m")
+        else:
+            print("o")
         return
     else:
-        temp = "moo" + "o" * depth
-        result += temp + before_str
-        calculate_s(depth + 1, result)
+        m_locate.append(str_length)
+        for i in range(len(m_locate) - 1):
+            m_locate.append(m_locate[i] + str_length + before_mid_len + 1)
+        calculate_s(m_locate, before_mid_len + 1)
 
 
 n = int(input())
-calculate_s(1, "moo")
+calculate_s([0], 3)
