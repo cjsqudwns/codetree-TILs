39d1c52a85532c58a82b115a8b3df87285890a7b
Cheon Byeongjun <59379460+cjsqudwns@users.noreply.github.com>
2024-06-29 12:15:27 +0900

    Title: 원점으로 돌아오기, Time: 253ms, Memory: 26MB, Status: Passed - CodeTree

--- 240629/원점으로 돌아오기/return-to-origin-point.py
+++ 240629/원점으로 돌아오기/return-to-origin-point.py
@@ -1,14 +1,41 @@
-def dps(x, y, cnt, rest_p):
+def dps(x, y, rest_p, prev_dir):
     if len(rest_p) == 0:
-        if x == 0 or y == 0:
+        check = False
+        if x == 0:
+            if y < 0 and prev_dir != 0:  # 상
+                check = True
+            elif y > 0 and prev_dir != 2:  # 하
+                check = True
+        elif y == 0:
+            if x < 0 and prev_dir != 1:  # 우
+                check = True
+            elif x > 0 and prev_dir != 3:  # 좌
+                check = True
+        if check:
             global ans
             ans += 1
-            return
+        return
     for p in rest_p:
-        if p[0] == x or p[1] == y:
-            next_p = rest_p.copy()
-            next_p.remove(p)
-            dps(p[0], p[1], cnt - 1, next_p)
+        dir = -1
+        if x == p[0]:
+            if y < p[1] and prev_dir != 0:  # 상
+                dir = 0
+            elif y > p[1] and prev_dir != 2:  # 하
+                dir = 2
+            else:
+                continue
+        elif y == p[1]:
+            if x < p[0] and prev_dir != 1:  # 우
+                dir = 1
+            elif x > p[0] and prev_dir != 3:  # 좌
+                dir = 3
+            else:
+                continue
+        else:
+            continue
+        next_p = rest_p.copy()
+        next_p.remove(p)
+        dps(p[0], p[1], next_p, dir)
 
 
 n = int(input())
@@ -16,5 +43,5 @@ p_arr = []
 for i in range(n):
     p_arr.append(tuple(map(int, input().split())))
 ans = 0
-dps(0, 0, n, p_arr.copy())
+dps(0, 0, p_arr.copy(), -1)
 print(ans)
